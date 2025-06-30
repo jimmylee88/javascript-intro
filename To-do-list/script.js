52679593553cfe746e7ed9a8066fbcc4db1fec9c
Jimmy Lee <jimmylee88@users.noreply.github.com>
2025-06-30 16:18:51 +0100

    Javascript added for storing data in local storage

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,3 +1,28 @@
 const inputBox = document.getElementById("input-box");
-const inputBox = document.getElementById('list-container');
+const listContainer = document.getElementById('list-container');
 
+
+function addTask() {
+    if (inputBox.value === ''){
+        alert("You can't add an empty task! Please write something.")
+    }
+
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "❌";
+        li.appendChild(span);
+    }
+    inputBox.value ="";
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false)
