074a9ec0cb6ebd614140e5968b2654c39fe0cfd8
Jimmy Lee <jimmylee88@users.noreply.github.com>
2025-07-09 23:15:36 +0100

    Created javascript file with initial variables & functions

---
+++ to-do-list-v2/script.js
@@ -0,0 +1,13 @@
+const addButton = document.getElementById("addTask");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+function addTask(){
+
+}
+
+function createTaskElement(task){
+    
+}
+
+// console.log(addEventListener)
